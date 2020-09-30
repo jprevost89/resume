3713f465b8ff298b6df4de8f67f805e63081fb51
JonPrevost <jonathan.prevost@mjfreeway.com>
2020-09-29 19:06:27 -0600

    Added state to About js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './About.css';
 
-const About = () => {
-    return(
-        <div className="row">
-            <div className="column">Test</div>
-            <div className="column">Content</div>
-        </div>
-    )
+class About extends Component {
+    state = {
+        name: "",
+        setName: false
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState({name: event.target.value})
+    }
+
+    nameSubmitHandler = () => {
+        this.setState({setName: true})
+    }
+
+    render() {
+        let nameIsSet = this.state.setName;
+        let formInput;
+        if(!nameIsSet) {
+            formInput= (
+                <form className="column">
+                    <label>
+                        Name:
+                        <input type="text" name="name" onChange={(e) => this.nameChangeHandler(e)} />
+                    </label>
+                    <button onClick={this.nameSubmitHandler}>Submit</button>
+                </form>
+            )
+        } else {
+            formInput = (
+                <div>
+                    <br/><br/>
+                    <h1>Hello {this.state.name}</h1>
+                </div>
+            )
+        }
+
+        return (
+            <div>{formInput}</div>
+        )
+    }
 };
 
 export default About;
