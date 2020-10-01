27a42e1895f7bdd014ac8da5faed2f5f373c8679
JonPrevost <jonathan.prevost@mjfreeway.com>
2020-09-30 18:26:11 -0600

    added icons to navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './Navbar.css'
+import './Navbar.css';
+import work from '../../Pictures/work.png';
+import home from '../../Pictures/home.png';
+import education from '../../Pictures/education.png';
+import contact from '../../Pictures/contact.png';
 
 const Navbar = () => {
     return (
         <div>
             <ul className="Nav">
-                <li><a href='/'>Home</a></li>
-                <li><a href='/experience'>Experience</a></li>
-                <li><a href='/education'>Education</a></li>
-                <li><a href='/contact'>Contact</a></li>
+                <li><a href='/'><img src={home}/>Home</a></li>
+                <li><a href='/experience'><img src={work}/>Experience</a></li>
+                <li><a href='/education'><img src={education}/>Education</a></li>
+                <li><a href='/contact'><img src={contact}/>Contact</a></li>
                 <p>Jon Prevost</p>
             </ul>
         </div>
